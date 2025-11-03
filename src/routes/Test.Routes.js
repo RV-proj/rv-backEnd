9c6c4bef802a15061408e4d58196c2a6d999cb53
Radwan Fahim <88280947+radwanfahim@users.noreply.github.com>
2025-11-03 22:57:36 +0600

    commit

--- src/routes/Test.Routes.js
+++ src/routes/Test.Routes.js
@@ -3,8 +3,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.send("Testing Api");
-
+  res.send("Testing Routes");
 });
 
 export default router;
