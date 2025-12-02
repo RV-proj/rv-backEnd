34cd4329bf9b191547363384658449d2ec3019d7
Abdullah Al Masrur <abdullahalmasrur8@gmail.com>
2025-12-03 00:49:42 +0600

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,4 +2,5 @@ import app from "./app.js";
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`https://rv-back-end.vercel.app:${PORT}`));
+
